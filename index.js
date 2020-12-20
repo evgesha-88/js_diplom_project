274e4eb085eadf434e1b8c92b34a88e7f61f854e
evgesha-88 <vlorien_2011@mail.ru>
2020-12-20 23:05:38 +0300

    Added modal

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-"use scrict"
+const modal = wmodal( {
+    title: "Go to main page",
+    closable: true,
+    content: `
+    <div class="input_layout">        
+        <input type="e-mail" id= "email" placeholder="Write your e-mail">  
+        <input type="password" id = "password" placeholder= "Write a password"> 
+    </div> 
+    `, 
+    width: '300px',
+    footerButtons: [
+        {text:'continue', type: 'primary', handler () {
+            console.log ("primary is clicked")
+        }},
+        {text:'cancel', type: 'danger', handler () {
+            console.log ("danger is clicked")
+        }},
+    ]
+        
+})
